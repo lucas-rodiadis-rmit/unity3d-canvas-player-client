30f4d5ccb897a3385dc5d742dc093ba8d8c78f04
s3943906 <s3943906@student.rmit.edu.au>
2025-04-10 17:10:23 +1000

    Created Div Elements in Control Bar (Matched to Mockup Design)

--- src/components/ControlBar.tsx
+++ src/components/ControlBar.tsx
@@ -3,7 +3,28 @@ import "./ControlBar.css";
 function ControlBar() {
 	return (
 		<div className="unity-player-control-bar">
-			<h1>Unity 3D Canvas Player</h1>
+			<div className="left-section">
+				<span>FPS: 30</span>
+				<div className="divider"></div>
+				<span>s1234567</span>
+			</div>
+
+			<div className="center-section">
+				<div className="logo">
+					<img src="unity-icon.svg" alt="Unity" style={{ height: '24px' }} />
+				</div>
+			</div>
+
+			<div className="right-section">
+				<div className="divider"></div>
+				<div className="icon-rotate" title="Restart"></div>
+				<div className="divider"></div>
+				<div className="icon-fullscreen" title="Fullscreen"></div>
+				<div className="divider"></div>
+				<div className="icon-menu" title="Menu"></div>
+				<div className="divider"></div>
+				<div className="icon-close" title="Close"></div>
+			</div>
 		</div>
 	);
 }
