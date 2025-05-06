c10cece47d0e7c5d495a6aef3730a7f630ce58c7
Natchanon Laoharawee <s4006840@student.rmit.edu.au>
2025-05-06 20:17:28 +1000

    WIP(fullscreen): added barebones functionality for fullscreen

--- src/components/ControlBar.tsx
+++ src/components/ControlBar.tsx
@@ -1,5 +1,23 @@
 import "./ControlBar.css";
 
+function MakeFullScreen() {
+	window.parent.postMessage(
+		{
+			subject: "requestFullWindowLaunch",
+			data: {
+				url: "https://canvasunityplayer.hudini.online/unity-player",
+				placement: "course_navigation",
+				launchType: "same_window",
+				launchOptions: {
+					width: 1000,
+					height: 800
+				}
+			}
+		},
+		"*"
+	);
+}
+
 function ControlBar() {
 	return (
 		<div className="unity-player-control-bar">
@@ -11,18 +29,39 @@ function ControlBar() {
 
 			<div className="center-section">
 				<div className="logo">
-					<img src="unity-logo.png" alt="Unity" style={{ height: '36px' }} />
+					<img
+						src="unity-logo.png"
+						alt="Unity"
+						style={{ height: "36px" }}
+					/>
 				</div>
 			</div>
 
 			<div className="right-section">
-				<img className="icon" title="Restart" src="reload-icon.png"></img>
+				<img
+					className="icon"
+					title="Restart"
+					src="reload-icon.png"
+				></img>
 				<div className="divider"></div>
-				<img className="icon" title="Fullscreen" src="fullscreen-icon.png"></img>
+				<img
+					className="icon"
+					title="Fullscreen"
+					src="fullscreen-icon.png"
+					onClick={MakeFullScreen}
+				></img>
 				<div className="divider"></div>
-				<img className="icon" title="Menu" src="options-icon.png"></img>
+				<img
+					className="icon"
+					title="Menu"
+					src="options-icon.png"
+				></img>
 				<div className="divider"></div>
-				<img className="icon" title="Close" src="cross-icon.png"></img>
+				<img
+					className="icon"
+					title="Close"
+					src="cross-icon.png"
+				></img>
 				<div className="divider"></div>
 			</div>
 		</div>
