0644dacc4ac69b0d20ec3654e520e62df6b3578b
lucas-rodiadis-rmit <s3935364@student.rmit.edu.au>
2025-05-09 12:51:22 +1000

    Removed react router

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,12 @@
 // src/main.tsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import App from './App.tsx';
-import XmlGenerator from './XmlGenerator.tsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/xml" element={<XmlGenerator />} />
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
 );
