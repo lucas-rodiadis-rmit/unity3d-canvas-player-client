91d3c1163ff2ca9ca5fafe94794a26eefa4a270c
Natchanon Laoharawee <s4006840@student.rmit.edu.au>
2025-05-11 23:32:29 +1000

    WIP(fixDirectories): separated client-side build logic to a client-side script

--- build_client_side.sh
+++ build_client_side.sh
@@ -1,2 +1,5 @@
+# Check if UNITY_PROJECTS_DIR is set, if not, exit with an error message
+: "${UNITY_PROJECTS_DIR:?UNITY_PROJECTS_DIR is not set}"
+
 # Build the client and copy into  
 npx vite build && cp -ra ./dist/* "../server/$UNITY_PROJECTS_DIR" &&
