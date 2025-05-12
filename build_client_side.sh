c2c9470cde6a751940af8aa88e9a8d2e21d43c43
Natchanon Laoharawee <s4006840@student.rmit.edu.au>
2025-05-12 19:11:54 +1000

    WIP(fixDirectories): added public assets copying

--- build_client_side.sh
+++ build_client_side.sh
@@ -1,5 +1,8 @@
 # Check if UNITY_PROJECTS_DIR is set, if not, exit with an error message
 : "${UNITY_PROJECTS_DIR:?UNITY_PROJECTS_DIR is not set}"
 
+# Copy public assets to respective server directory
+cp -ra ./public "../server/src/public" &&
+
 # Build the client and copy into  
 npx vite build && cp -ra ./dist/* "../server/$UNITY_PROJECTS_DIR" &&
