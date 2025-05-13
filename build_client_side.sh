15498e37dc98d88e37c906df21750a696b4d4629
Natchanon Laoharawee <s4006840@student.rmit.edu.au>
2025-05-13 18:04:55 +1000

    fix(fixDirectories): removed redundant && operator in script

--- build_client_side.sh
+++ build_client_side.sh
@@ -12,4 +12,4 @@ EOF
 cp -ra ./public "../server/src/public" &&
 
 # Build the client and copy into  
-npx vite build && cp -ra ./dist/* "../server/$UNITY_PROJECTS_DIR" &&
+npx vite build && cp -ra ./dist/* "../server/$UNITY_PROJECTS_DIR"
