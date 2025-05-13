77de13d591a1145bb91ac9d2b5439bfbf71dc8dc
Natchanon Laoharawee <s4006840@student.rmit.edu.au>
2025-05-13 18:52:30 +1000

    fix(fixDirectories): had script change directories

--- build_client_side.sh
+++ build_client_side.sh
@@ -1,5 +1,8 @@
 # Check if UNITY_PROJECTS_DIR is set, if not, exit with an error message
-: "${UNITY_PROJECTS_DIR:?UNITY_PROJECTS_DIR is not set}"
+: "${UNITY_PROJECTS_DIR:?UNITY_PROJECTS_DIR is not set}" &&
+
+# Change working directory to the directory of this script
+cd "$(dirname "$0")" &&
 
 # Write/update Vite-compatible .env file in the client directory
 # so that variables can be injected into the client-side code
@@ -12,4 +15,7 @@ EOF
 cp -ra ./public "../server/src/public" &&
 
 # Build the client and copy into  
-npx vite build && cp -ra ./dist/* "../server/$UNITY_PROJECTS_DIR"
+npx vite build && cp -ra ./dist/* "../server/$UNITY_PROJECTS_DIR" &&
+
+# Return to the original directory
+cd $PARENT_SCRIPT_DIR
