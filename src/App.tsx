d2fd2f4173dd27911fbd74bbf178e8d0b71e09d9
s3905743HaoranZhang <s3905743@student.rmit.edu.au>
2025-05-15 00:19:24 +1000

    updated embedselection page

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,35 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 
 import ControlBar from "./components/ControlBar";
 import UnityPlayer, {
-	DefaultUnityPlayerConfig
+  DefaultUnityPlayerConfig
 } from "./components/UnityPlayer";
 
 function App() {
-	const [auth, setAuth] = useState(true);
+  const auth = true;
 
-	const [player, setA] = useState<React.JSX | null>(null);
-	useEffect(() => {
-		setA(
-			<UnityPlayer
-				url="build"
-				config={DefaultUnityPlayerConfig}
-			/>
-		);
-	}, []);
+  const [player, setPlayer] = useState<React.ReactElement | null>(null);
 
-	return (
-		<>
-			<div className="unity-player-main">
-				<ControlBar />
+  useEffect(() => {
+    setPlayer(
+      <UnityPlayer
+        url="build"
+        config={DefaultUnityPlayerConfig}
+      />
+    );
+  }, []);
 
-				{auth ? (
-					player || (
-						<div>No player available.</div>
-					)
-				) : (
-					<div>
-						You are not authorised to view this
-						content.
-					</div>
-				)}
-			</div>
-		</>
-	);
+  return (
+    <div className="unity-player-main">
+      <ControlBar />
+      {auth ? (
+        player || <div>No player available.</div>
+      ) : (
+        <div>You are not authorised to view this content.</div>
+      )}
+    </div>
+  );
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import App from './App.tsx';
 import XmlGenerator from './XmlGenerator.tsx';
+import EmbedSelection from './EmbedSelection.tsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
@@ -13,6 +14,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/xml" element={<XmlGenerator />} />
+        <Route path="/embed" element={<EmbedSelection />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
