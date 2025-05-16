ed7838e45322100d446a86acc4cc30f843123da1
s3943906 <s3943906@student.rmit.edu.au>
2025-05-16 13:46:33 +1000

    Added Control Bar Toggle

--- src/components/ControlBar.tsx
+++ src/components/ControlBar.tsx
@@ -7,47 +7,44 @@ function ControlBar() {
 
 	return (
 		<div>
-			{!visible && <div className="center-section" onClick={toggleVisible}>
-				<div className="logo">
-					<img
-						src="unity-logo.png"
-						alt="Unity"
-						style={{ height: "36px", cursor: "pointer" }}
-					/>
-				</div>
+			{!visible && <div className="center-section">
+				<div className="control-bar-toggle-button" onClick={toggleVisible}></div>
 			</div>}
 
 			{visible && (
-				<div className="unity-player-control-bar">
-					<div className="left-section">
-						<span>FPS: 30</span>
-						<div className="divider"></div>
-						<span>s1234567</span>
-					</div>
+				<div>
+					<div className="unity-player-control-bar">
+						<div className="left-section">
+							<span>FPS: 30</span>
+							<div className="divider"></div>
+							<span>s1234567</span>
+						</div>
 
-					<div className="center-section" onClick={toggleVisible}>
-						<div className="logo">
-							<img
-								src="unity-logo.png"
-								alt="Unity"
-								style={{ height: "36px", cursor: "pointer" }}
-							/>
+						<div className="center-section" onClick={toggleVisible}>
+							<div className="logo">
+								<img
+									src="unity-logo.png"
+									alt="Unity"
+									style={{ height: "36px", cursor: "pointer" }}
+								/>
+							</div>
+						</div>
+
+
+						<div className="right-section">
+							<img className="icon" title="Restart" src="reload-icon.png" />
+							<div className="divider"></div>
+							<img className="icon" title="Fullscreen" src="fullscreen-icon.png" />
+							<div className="divider"></div>
+							<img className="icon" title="Menu" src="options-icon.png" />
+							<div className="divider"></div>
+							<img className="icon" title="Close" src="cross-icon.png" />
+							<div className="divider"></div>
 						</div>
 					</div>
+					<div className="control-bar-toggle-button" onClick={toggleVisible}></div>
 
-
-					<div className="right-section">
-						<img className="icon" title="Restart" src="reload-icon.png" />
-						<div className="divider"></div>
-						<img className="icon" title="Fullscreen" src="fullscreen-icon.png" />
-						<div className="divider"></div>
-						<img className="icon" title="Menu" src="options-icon.png" />
-						<div className="divider"></div>
-						<img className="icon" title="Close" src="cross-icon.png" />
-						<div className="divider"></div>
-					</div>
-				</div>
-			)}
+				</div>)}
 		</div>
 	);
 }
