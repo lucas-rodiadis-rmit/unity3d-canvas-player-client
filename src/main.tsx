40399e345f16671fc6739931363e2897757215bd
lucas-rodiadis-rmit <s3935364@student.rmit.edu.au>
2025-05-18 20:34:12 +1000

    Re-enabled front end routes

--- src/main.tsx
+++ src/main.tsx
@@ -7,17 +7,17 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import App from './App.tsx';
-// import EmbedSelection from './pages/EmbedSelection.tsx';
-// import XMLGenerator from './pages/XMLGenerator.tsx';
+import XMLGenerator from './pages/XMLGenerator.tsx';
+import EmbedSelection from './pages/EmbedSelection.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
 		<BrowserRouter>
 			<Routes>
 				<Route path="/" element={<>hello </>} />
-				{/* <Route path="/register" element={<XMLGenerator />} /> */}
+				<Route path="/register" element={<XMLGenerator />} />
 				<Route path="/unity-player/:project_id" element={<App />} />
-				{/* <Route path="/embed" element={<EmbedSelection />} /> */}
+				<Route path="/embed" element={<EmbedSelection />} />
 				<Route path="*" element={<>Invalid route.</>} />
 			</Routes>
 		</BrowserRouter>
