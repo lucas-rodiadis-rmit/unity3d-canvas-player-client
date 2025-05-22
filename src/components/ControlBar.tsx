b3dbaa0d7666db903fc9e85e98834ad6a3803570
s3943906 <s3943906@student.rmit.edu.au>
2025-05-22 17:38:14 +1000

    Added Toggle Animations

--- src/components/ControlBar.tsx
+++ src/components/ControlBar.tsx
@@ -12,68 +12,58 @@ function ControlBar({ makeFullScreen }: ControlBarProps) {
 	const toggleVisible = () => setVisible(!visible);
 
 	return (
-		<div className="control-bar-group">
-			{!visible && (
-				<div className="center-section">
-					<div className="control-bar-toggle-button" onClick={toggleVisible}></div>
+		<div className={`control-bar-group ${visible ? "visible" : "hidden"}`}>
+			<div className={`unity-player-control-bar ${visible ? "fade-in" : "fade-out"}`}>
+				<div className="left-section">
+					<span>FPS: 30</span>
+					<div className="divider"></div>
+					<span>s1234567</span>
 				</div>
-			)}
 
-			{visible && (
-				<div className="control-bar-group">
-					<div className="unity-player-control-bar">
-						<div className="left-section">
-							<span>FPS: 30</span>
-							<div className="divider"></div>
-							<span>s1234567</span>
-						</div>
-
-						<div
-							className="center-section"
-							onClick={toggleVisible}
-						>
-							<div className="logo">
-								<img
-									src={`/images//unity-logo.png`}
-									alt="Unity"
-									style={{
-										height: "36px",
-										cursor: "pointer"
-									}}
-								/>
-							</div>
-						</div>
-
-						<div className="right-section">
-							<img
-								className="icon"
-								title="Restart"
-								src={`/images/reload-icon.png`}
-							/>
-							<div className="divider"></div>
-							<img
-								className="icon"
-								title="Fullscreen"
-								src={`/images/fullscreen-icon.png`}
-								onClick={() => makeFullScreen()}
-							></img>
-							<div className="divider"></div>
-							<img
-								className="icon"
-								title="Menu"
-								src={`/images/options-icon.png`}
-							></img>
-							<div className="divider"></div>
-							<img
-								className="icon"
-								title="Close"
-								src={`/images/cross-icon.png`}
-							></img>
-						</div>
+				<div
+					className="center-section"
+					onClick={toggleVisible}
+				>
+					<div className="logo">
+						<img
+							src={`/images//unity-logo.png`}
+							alt="Unity"
+							style={{
+								height: "36px",
+								cursor: "pointer"
+							}}
+						/>
 					</div>
-					<div className="control-bar-toggle-button" onClick={toggleVisible}></div>
 				</div>
-			)}
+
+				<div className="right-section">
+					<img
+						className="icon"
+						title="Restart"
+						src={`/images/reload-icon.png`}
+					/>
+					<div className="divider"></div>
+					<img
+						className="icon"
+						title="Fullscreen"
+						src={`/images/fullscreen-icon.png`}
+						onClick={() => makeFullScreen()}
+					></img>
+					<div className="divider"></div>
+					<img
+						className="icon"
+						title="Menu"
+						src={`/images/options-icon.png`}
+					></img>
+					<div className="divider"></div>
+					<img
+						className="icon"
+						title="Close"
+						src={`/images/cross-icon.png`}
+					></img>
+				</div>
+			</div>
+			<div className={`control-bar-toggle-button ${visible ? " visible" : "hidden"}`} onClick={toggleVisible} ></div>
 		</div>
 	);
 }
